02c9bdb95564fc0f0d69e23b8b43680222093ada
Oghenemeru Avwemoya <skiboss4life@gmail.com>
2022-05-20 17:48:28 -0700

    Update simple shell project

--- shell.c
+++ shell.c
@@ -1,88 +1,27 @@
 #include "main.h"
-
 /**
- * execute - executes the command
- * @cmd: command to run
- * Return: 0 on success1 -1 if cmd is exit and 1 on any other error
+ * main - shell skeleton
+ * @argc: number of arguments in the input
+ * @argv: arguments in the input
+ * @envp: environment variables
+ * Return: 0 on Success
  */
-int execute(char **cmd)
+int main(int argc, char *argv[], char *envp[])
 {
 
-	pid_t child_pid;
-	int status;
-
-	if (strncmp("exit", cmd[0], 4) == 0)
-		return (-1);
-
-	child_pid = fork();
-
-	if (child_pid == -1)
+	if (argc == 1)
 	{
-		perror("Error");
-		return (1);
+		_noargv(argv, envp);
 	}
-	else if (child_pid == 0)
+	else if (argc == 2)
 	{
-		if (execve(cmd[0], cmd, NULL) == -1)
-		{
-			perror("Error");
-			exit(-1);
-		}
+		_yesargv(argv, envp);
 	}
 	else
-		wait(&status);
-
-	return (0);
-}
-
-
-/**
- * main - main simple shell
- * @argc: number of arguments
- * @argv: list of command line arguments
- * Return: Always 0, -1 on error.
- */
-
-int main(int argc, char **argv)
-{
-
-	int response;
-	char **tokens;
-	size_t bufsize = BUFSIZ;
-	int isPipe = 0;
-	char *buffer;
-
-	if (argc >= 2)
 	{
-		/*TODO: Handle cases where there is no argument, only the command*/
-		if (execve(argv[1], argv, NULL) == -1)
-		{
-			perror("Error");
-			exit(-1);
-		}
-		return (0);
+		write(STDIN_FILENO, "NO ADMITTED AMOUNT OF ARGUMENTS", 31);
+		write(STDIN_FILENO, "\n", 1);
 	}
-
-	buffer = (char *)malloc(bufsize * sizeof(char));
-	if (buffer == NULL)
-	{
-		perror("Unable to allocate buffer");
-		exit(1);
-	}
-
-	do {
-		if (isatty(fileno(stdin)))
-		{
-			isPipe = 1;
-			_puts("cisfun#: ");
-		}
-
-		getline(&buffer, &bufsize, stdin);
-		buffer[_strlen(buffer) - 1] = '\0';
-		tokens = stringToTokens(buffer);
-		response = execute(tokens);
-	} while (isPipe && response != -1);
-
 	return (0);
 }
 
